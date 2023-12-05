609b89d1a586d263d02e7736bdbfeb09371f3735
Abhishek Singh <Abhisheksiingh5261@gmail.com>
2023-12-05 13:25:12 +0530

    final commit

---
+++ Script.js
@@ -0,0 +1,44 @@
+const btn = document.querySelector('#emoji');
+const emojis = [
+  '😆',
+  '😅',
+  '🤣',
+  '😂',
+  '😀',
+  '🤑',
+  '🤨',
+  '🙂',
+  '😊',
+  '😗',
+  '😛',
+  '😏',
+  '🤥',
+  '😴',
+  '🥺',
+  '😧',
+  '😇',
+  '😳',
+  '🙃',
+  '🥴',
+  '🧐',
+  '🤨',
+  '😒',
+  '🤔',
+  '🤭',
+  '🥰',
+  '🤐',
+  '👀',
+  '🤔',
+  '🤪',
+  '🥲',
+  '😃',
+  '😁',
+  '😬',
+];
+
+btn.addEventListener("mouseenter", function() {
+    let randomindex = Math.floor( Math.random() * emojis.length )
+    console.log(randomindex);
+    let newEmoji = emojis[randomindex];
+    btn.innerHTML = newEmoji;
+})
